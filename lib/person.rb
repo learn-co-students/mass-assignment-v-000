a3d51b2d8bd357eb61a72f9b5cd1be06ad194f29
Chris Bruen <bruen.chris@gmail.com>
2016-01-03 14:31:16 -0500

    try again

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   def initialize(attributes)
-  	attributes.each { |key,value| self.send("#{key}=", value) }
   	attributes.each { |key, value| attr_accessor key }
+  	attributes.each { |key,value| self.send("#{key}=", value) }
   end
 end
