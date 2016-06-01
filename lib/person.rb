44f65ce723dd783421e4a5c707aaefd0a01d91a7
Being Young <beingy@gmail.com>
2016-06-01 14:45:14 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,8 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(attributes = nil)
+    attributes.each {|key, value| self.send(("#{key}="), value)} if attributes
   end
 
 end
-
-# bob_attributes = {name: "Bob", age: 27, hair_color: "Brown"}
-#
-# bob = Person.new(bob_attributes)
-# bob.name       # => "Bob"
-# bob.age        # => 27
-# bob.hair_color # => "Brown"
-#
-# susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
-#
-# susan = Person.new(susan_attributes)
-# susan.name      # => "Susan"
-# susan.height    # => "5'11""
-# susan.eye_color # => "Green"
