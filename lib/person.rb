8a3b1ca55e8f3c0bce2ae2bd3657069a0eedf954
BriOD <briodonoghue@gmail.com>
2016-07-31 13:05:30 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize(a_hash) 
+    a_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+
+  end
+
+
+    
+  
+
   #your code here
 end
