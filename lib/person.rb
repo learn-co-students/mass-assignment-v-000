ff99aa7300e40aad739cbce792d479ed36fd26ed
Terry Thompson <terry.lyn.thompson@gmail.com>
2017-01-22 00:54:11 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  #your code here
-end
+attr_accessor :name, :birthday, :hair_color, :eye_color,
+:height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
+:glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end# of do
+  end# of initialize
+
+
+end# of class 
