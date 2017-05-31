e6f4750e50c5eaefbbb8df725bc5f7c51754eed7
Samson Yuwono <samson.yuwono@gmail.com>
2017-05-31 16:00:17 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each{|key, value| self.send(("#{key}="), value)}
-end
+    attributes.each{|key, value| self.send(("#{key}="), value)} #looks at key and the value assingned to it
+end #instance_of_user.name = "Sophie" = sophie.name = "Sophie"
 
 end
