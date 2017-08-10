e0001f64071a341c0367842e1e5425fe39242d59
roitblatari <roitblatari@gmail.com>
2017-08-10 15:49:53 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each{|k,v| self.send("#{k}=",v)}
+    attributes.each{|key,value| self.send("#{key}=",value)}
   end
 
 end
