5b5deb02834c1dcfe142b77e5cdfa8d2fbe2da9f
Naomi Dennis <njdennis94@gmail.com>
2017-09-07 17:16:24 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
+=begin
+  Attributes (all public):
+  :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  Use the send method to assign values to the class
+=end
+
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    self.name = "something"
+    attributes.each do |key, value|
+        self.send(("#{key}="), value)
+    end
+  end
+
 end
