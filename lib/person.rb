b47fc21a3d159b2315e26c5a3e98e9e7f88bd8b5
Blakeley Bond <burrick2003@yahoo.com>
2017-11-21 04:40:17 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  #your code here
-end
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize3(attributes) #from the lesson
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  def initialize(attributes) #dissected
+    attributes.each do |key, value| #doesn't this only work insofar as you already know the keys in order to have them in attr_ccessor?  I wonder if you can have a class method the can build an attr_accessor from what's input
+      self.send(("#{key}="), value) #for each key/value pair in the input hash, sets @birthday/self.birthday = value
+    end
+  end
+
+    end
