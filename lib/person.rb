71e5cd91249dd9660f2d2b1ff7e711078ce79e56
kostanzhoglo <jeffreywithers@gmail.com>
2018-01-16 06:23:48 -0500

    Add metaprogramming initialize method

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
   #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :wrist_size, :glove_size, :pant_length, :pant_width, :handed, :complexion, :t_shirt_size
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+  end
+
 end
