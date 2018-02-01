397cbb125adc17b9e95627e11d20b9c4b3f34330
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-01 20:16:06 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  #your code here
+  
+  def initialize(props)
+    props.each do |k,v|
+      self.class.send(:attr_accessor,k)
+      instance_variable_set("@#{k}",v)
+    end
+  end
+  
 end
