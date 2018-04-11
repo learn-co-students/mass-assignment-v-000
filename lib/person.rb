9935f558ee410b4a55c9b0f247c60e40e594d727
Hiroki Kato <hkato@sas.upenn.edu>
2018-04-11 18:09:52 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,14 +2,16 @@ require 'pry'
 
 class Person
 
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
+  def initialize(attributes=nil)
 # binding.pry
-    attributes.each do |key, value|
-      self.send("#{key}=", value)
+    if attributes
+      attributes.each do |key, value|
+        self.send("#{key}=", value)
+      end
     end
   end
 
