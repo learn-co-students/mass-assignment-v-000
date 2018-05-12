6d964f2724bae01cc26782d4da0ad7f73b550336
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-12 02:50:42 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -9,3 +9,16 @@ class Person
     end
   end
 end
+
+=begin
+class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes=nil)
+    if attributes
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
+  end
+end
+=end
