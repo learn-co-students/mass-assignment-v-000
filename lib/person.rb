635a11acd3d0d93d5eb05f19337e08b36e3ea594
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-09 09:32:00 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  
+  attr_accessor :birthday, :weight, :handed, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width, :name, :hair_color, :eye_color, :height, :complexion 
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end 
 end
