583f5fd1a1a1eea55cf00b3d04a099349178df3a
hkato4188 <hkato@sas.upenn.edu>
2020-08-24 15:52:04 -0400

    added initialize method with metaprogramming

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,12 @@
-require 'pry'
-
 class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-:weight, :handed, :complexion, :t_shirt_size,
-:wrist_size, :glove_size, :pant_length, :pant_width
+  :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
+  :glove_size, :pant_length, :pant_width
 
   def initialize(attributes=nil)
-# binding.pry
     if attributes
-      attributes.each do |key, value|
-        self.send("#{key}=", value)
-      end
+      attributes.each {|key, value| self.send("#{key}=", value)}
     end
   end
-
 end
